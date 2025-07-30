0605a6da1b52020574040dd4fa767f103e875c59
Phillip Haftings <phillip.haftings@example.com>
2025-07-30 06:25:25 -0400

    legacy ASCII/binary output for vlbi.difx.visibilities
    - Also added version to Record class
    - Also documented properties of Record class
    - Also made Record.pack choose original format as default
    - Also tested both binary, and ASCII/binary (success)

--- vlbi/difx/visibilities.py
+++ vlbi/difx/visibilities.py
@@ -10,46 +10,99 @@ BINARY_FILE = BufferedIOBase | RawIOBase
 STRUCT_RECORD = Struct('<IIdIII2sIdddd')
 
 
+def _format_num(num: float | int) -> str:
+    '''Format a number for DiFX visibility ASCII output'''
+    return str(num if num % 1 else int(num))
+
+
 @dataclass
 class Record:
     '''DiFX visibility header and data'''
-    baseline: tuple[int, int] = (0, 0)
-    mjd: int = 0
-    seconds: float = 0.0
-    config: int = 0
-    source: int = 0
-    freq: int = 0
-    pols: str = 'RR'
-    pulsar: int = 0
-    flagged: int = 0
-    weight: float = 0.0
-    u: float = 0.0
-    v: float = 0.0
-    w: float = 0.0
-    visibilities: list[complex] = field(default_factory=list)
 
-    def pack(self, version: int = 1) -> bytes:
-        '''Serialize record into bytes for output to DiFX file'''
+    format: int = 1
+    '''format version, 1 for binary, 0 for legacy mixed ASCII/binary'''
+
+    baseline: tuple[int, int] = (0, 0)
+    '''1-based station indexes'''
+
+    mjd: int = 0
+    '''modified Julian date (MJD)'''
+
+    seconds: float = 0.0
+    '''fractional seconds of MJD'''
+
+    config: int = 0
+    '''configuration index (from input file)'''
+
+    source: int = 0
+    '''source index (from calc file)'''
+
+    freq: int = 0
+    '''frequency index (from input file)'''
+
+    pols: str = 'RR'
+    '''polarizations pair'''
+
+    pulsar: int = 0
+    '''pulsar bin index (from pulsar file)'''
+
+    flagged: int = 0
+    '''legacy flagging indicator, only present in mixed ASCII/binary files'''
+
+    weight: float = 0.0
+    '''data weighting factor'''
+
+    u: float = 0.0
+    '''U in meters'''
+
+    v: float = 0.0
+    '''V in meters'''
+
+    w: float = 0.0
+    '''W in meters'''
+
+    visibilities: list[complex] = field(default_factory=list)
+    '''complex visibilities for each channel'''
+
+
+    def pack(self, format: int | None = None) -> bytes:
+        '''Serialize record into bytes for output to DiFX file
+
+        `format` is 0 for mixed ASCII/binary, 1 for binary, None for auto
+        '''
+        format = self.format if format is None else format
+        baseline = self.baseline[0] * 256 + self.baseline[1]
+        visibilities = _pack('<' + 2 * len(self.visibilities) * 'f', *(
+            i for x in self.visibilities for i in (x.real, x.imag)
+        ))
         # binary version
-        if version == 1:
-            baseline = self.baseline[0] * 256 + self.baseline[1]
+        if format == 1:
             return b'\x00\xff\x00\xff\x01\x00\x00\x00' + STRUCT_RECORD.pack(
                 baseline, self.mjd, self.seconds,
                 self.config, self.source, self.freq,
                 self.pols.encode('utf-8').ljust(2),
                 self.pulsar, self.weight, self.u, self.v, self.w
-            ) + _pack('<' + 2 * len(self.visibilities) * 'f', *(
-                i for x in self.visibilities for i in (x.real, x.imag)
-            ))
-            # TODO test binary output against read-in (should be exact match)
+            ) + visibilities
         # mixed ASCII version
-        elif version == 0:
-            msg = 'mixed ASCII/binary output mode not yet supported'
-            raise NotImplementedError(msg)
-            ...  # TODO output mixed ASCII version
+        elif format == 0:
+            return (
+                f'BASELINE NUM:       {baseline}\n'
+                f'MJD:                {self.mjd}\n'
+                f'SECONDS:            {_format_num(self.seconds)}\n'
+                f'CONFIG INDEX:       {self.config}\n'
+                f'SOURCE INDEX:       {self.source}\n'
+                f'FREQ INDEX:         {self.freq}\n'
+                f'POLARISATION PAIR:  {self.pols}\n'
+                f'PULSAR BIN:         {self.pulsar}\n'
+                f'FLAGGED:            {self.flagged}\n'
+                f'DATA WEIGHT:        {_format_num(self.weight)}\n'
+                f'U (METRES):         {_format_num(self.u)}\n'
+                f'V (METRES):         {_format_num(self.v)}\n'
+                f'W (METRES):         {_format_num(self.w)}\n'
+            ).encode('utf-8') + visibilities
         # unsupported version
         else:
-            msg = f'Unsupported DiFX visibilities version: {version}'
+            msg = f'Unsupported DiFX visibilities format version: {format}'
             raise ValueError(msg)
 
     def write(self, file: BINARY_FILE, version: int = 1) -> int:
@@ -94,8 +147,8 @@ def records(
                 bl = baseline // 256, baseline % 256
                 try:
                     yield Record(
-                        bl, mjd, sec, config, src, freq, pols.decode('utf-8'),
-                        pulsar, 0, weight, u, v, w, x
+                        1, bl, mjd, sec, config, src, freq,
+                        pols.decode('utf-8'), pulsar, 0, weight, u, v, w, x
                     )
                 except ValueError:
                     raise ValueError('Unrecognized data in DiFX visibilities')
@@ -103,18 +156,10 @@ def records(
                 values = []
                 for key in (
                     b'NUM',  # truncated because 'BASELINE' already read
-                    b'MJD',
-                    b'SECONDS',
-                    b'CONFIG INDEX',
-                    b'SOURCE INDEX',
-                    b'FREQ INDEX',
-                    b'POLARISATION PAIR',
-                    b'PULSAR BIN',
-                    b'FLAGGED',
-                    b'DATA WEIGHT',
-                    b'U (METRES)',
-                    b'V (METRES)',
-                    b'W (METRES)'
+                    b'MJD', b'SECONDS',
+                    b'CONFIG INDEX', b'SOURCE INDEX', b'FREQ INDEX',
+                    b'POLARISATION PAIR', b'PULSAR BIN', b'FLAGGED',
+                    b'DATA WEIGHT', b'U (METRES)', b'V (METRES)', b'W (METRES)'
                 ):
                     k, _, value = source.readline().partition(b':')
                     if k.strip() != key:
@@ -135,20 +180,10 @@ def records(
                     baseline = int(baseline)
                     bl = baseline // 256, baseline % 256
                     yield Record(
-                        bl,
-                        int(mjd),
-                        float(sec),
-                        int(config),
-                        int(src),
-                        int(freq),
-                        pols.strip().decode('utf-8'),
-                        int(pulsar),
-                        int(flagged),
-                        float(weight),
-                        float(u),
-                        float(v),
-                        float(w),
-                        x
+                        0, bl, int(mjd), float(sec), int(config), int(src),
+                        int(freq), pols.strip().decode('utf-8'),
+                        int(pulsar), int(flagged), float(weight),
+                        float(u), float(v), float(w), x
                     )
                 except ValueError:
                     raise ValueError('Unrecognized data in DiFX visibilities')
